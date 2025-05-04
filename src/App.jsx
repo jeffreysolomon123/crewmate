98d8f8aa0bf54f7144bbf5ce4414ef70add4204f
jeffreysolomon123 <jeffreysolomon.official@gmail.com>
2025-05-04 22:28:47 +0530

    fix:onboard page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from "./Pages/Home"
 import Explore from './Pages/Explore';
-import Onboard from "./Pages/Onboard"
 import Login from './Pages/Login';
 import SignUp from './Pages/SignUp';
 import Dashboard from './Pages/Dashboard';
@@ -24,7 +23,6 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/explore' element={<Explore />} />
-            <Route path='/onboard' element={<Onboard />} />   
             <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<SignUp />} />
             <Route path='/dashboard' element={<Dashboard />} />
